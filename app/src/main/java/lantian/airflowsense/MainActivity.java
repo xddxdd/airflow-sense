4f9d29249fe28dd53f491fac6aa62460acadb6da
ZhuKerui <kerui.17@intl.zju.edu.cn>
2020-02-06 23:38:32 +0800

    new layout with weather working

--- app/src/main/java/lantian/airflowsense/MainActivity.java
+++ app/src/main/java/lantian/airflowsense/MainActivity.java
@@ -19,19 +19,72 @@ import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import lantian.airflowsense.receiver.BLEReceiveService;
 import lantian.airflowsense.weather.WeatherCallback;
 import lantian.airflowsense.weather.WeatherData;
 import lantian.airflowsense.weather.WeatherHelper;
 
 public class MainActivity extends AppCompatActivity {
-    DataUpdateReceiver dataUpdateReceiver = new DataUpdateReceiver();
-    IntentFilter intentFilter = new IntentFilter();
-    WeatherHelper weatherHelper = new WeatherHelper();
 
+    WeatherHelper weatherHelper = new WeatherHelper(); // A manager that get weather information from HeWeather App
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        refreshWeather();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                return true;
+            case R.id.menu_upload:
+                refreshWeather();
+                break;
+            case R.id.menu_delete:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * refreshWeather
+     * Refresh the weather information when needed
+     */
     private void refreshWeather() {
-        findViewById(R.id.button_refresh_weather).setEnabled(false);
 
+        /* Get the entities of weather related TextView components */
         final TextView textWeather = findViewById(R.id.text_weather);
         final TextView textUpdateTime = findViewById(R.id.text_update_time);
         final TextView textTemperature = findViewById(R.id.text_temperature);
@@ -39,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         final TextView textHumidity = findViewById(R.id.text_humidity);
         final TextView textAQI = findViewById(R.id.text_aqi);
 
+        /* Print the waiting information */
         textWeather.setText("正在刷新");
         textUpdateTime.setText("---");
         textTemperature.setText("--");
@@ -46,12 +100,16 @@ public class MainActivity extends AppCompatActivity {
         textHumidity.setText("--");
         textAQI.setText("---");
 
-        weatherHelper.fetchWeatherAsync(this, new WeatherCallback() {
+        /* Get the information */
+        weatherHelper.fetchWeatherAsync(this, new WeatherCallback() { // WeatherCallback is an abstract class with only a callback function header
             @Override
             public void callback(final WeatherData data) {
+                /* Run something on the main thread (UI Thread) */
                 runOnUiThread(new Runnable() {
                     @Override
+                    /* Code to be run on main thread */
                     public void run() {
+                        /* Display the data/failure information onto the screen */
                         try {
                             if (data.isReady()) {
                                 textWeather.setText(data.weather);
@@ -59,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                                 textTemperature.setText(data.temperature);
                                 textLocation.setText(data.location);
                                 textHumidity.setText(data.humidity);
-                                textAQI.setText(data.aqi + " (" + data.aqi_level + ")");
+                                textAQI.setText(String.format("%s (%s)", data.aqi, data.aqi_level));
                             } else {
                                 textWeather.setText("获取失败");
                                 textUpdateTime.setText("---");
@@ -72,156 +130,9 @@ public class MainActivity extends AppCompatActivity {
                         } catch (NullPointerException npe) {
                             npe.printStackTrace();
                         }
-                        findViewById(R.id.button_refresh_weather).setEnabled(true);
                     }
                 });
             }
         });
-    }
-
-    private BLEReceiveService serviceInstance;
-
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            serviceInstance = ((BLEReceiveService.MyBinder) service).getService();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            serviceInstance = null;
-        }
-    };
-
-    private void startDataReceiveService() {
-        startService(new Intent(this, BLEReceiveService.class));
-    }
-
-    private void stopDataReceiveService() {
-        stopService(new Intent(this, BLEReceiveService.class));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_connect:
-                startDataReceiveService();
-                return true;
-            case R.id.menu_disconnect:
-                stopDataReceiveService();
-                return true;
-            default:
-                return false;
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        intentFilter.addAction(Common.BROADCAST_DATA_UPDATE);
-        intentFilter.addAction(Common.BROADCAST_CONNECTION_STATUS_UPDATE);
-        registerReceiver(dataUpdateReceiver, intentFilter);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-//        startDataReceiveService();
-
-        findViewById(R.id.button_refresh_weather).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                refreshWeather();
-            }
-        });
-
-        refreshWeather();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(dataUpdateReceiver);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(dataUpdateReceiver, intentFilter);
-//        startDataReceiveService();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        stopDataReceiveService();
-    }
-
-    public class DataUpdateReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if(Common.BROADCAST_DATA_UPDATE.equals(intent.getAction())) {
-                final double new_value = intent.getDoubleExtra("new_value", 0.0);
-                final DataPlotView dpv = findViewById(R.id.dataplot);
-
-                // Ignore the latest data point in calculation purposely
-                // To improve UI drawing performance
-                // (Data inaccuracy here is insignificant anyways)
-                final String recent_max = String.format(
-                        Locale.SIMPLIFIED_CHINESE,
-                        "%.2f",
-                        dpv.getRecentDataMax(0)
-                );
-                final String recent_min = String.format(
-                        Locale.SIMPLIFIED_CHINESE,
-                        "%.2f",
-                        dpv.getRecentDataMin(0)
-                );
-                final String recent_average = String.format(
-                        Locale.SIMPLIFIED_CHINESE,
-                        "%.2f",
-                        dpv.getRecentDataAverage(1)
-                );
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        dpv.addDataPoint(new_value);
-                        ((TextView) findViewById(R.id.textbox)).setText(recent_average);
-                        ((TextView) findViewById(R.id.text_max_value)).setText(recent_max);
-                        ((TextView) findViewById(R.id.text_min_value)).setText(recent_min);
-                        ((TextView) findViewById(R.id.text_average_value)).setText(recent_average);
-
-                        ToggleButton tb = findViewById(R.id.measurement_button);
-                        if (tb.isChecked()) {
-                            ((TextView) findViewById(R.id.text_measurement_value)).setText(recent_max);
-                        }
-                    }
-                });
-            } else if(Common.BROADCAST_CONNECTION_STATUS_UPDATE.equals(intent.getAction())) {
-                final boolean connected = intent.getBooleanExtra("connected", false);
-                final String name = intent.getStringExtra("name");
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(connected) {
-//                            getSupportActionBar().setTitle("呼吸监测设备 已连接");
-                            getSupportActionBar().setTitle(name + " 已连接");
-                        } else {
-//                            getSupportActionBar().setTitle("呼吸监测设备 连接中断");
-                            getSupportActionBar().setTitle(name + " 连接中断");
-                        }
-                    }
-                });
-            }
-        }
     }
 }
